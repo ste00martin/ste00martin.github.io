7ecf9c66a9fb03b13c5550a4f01ec70bac349878
Stefan Martin <ste00martin@gmail.com>
2024-04-11 11:48:55 -0700

    adding link to discord

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+// creation over consumption
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          creation over consumption
+          building friendships through shared interests
         </p>
+        <a style={{color: '#FEA700', fontSize: 14, textDecoration: 'none' }} href="https://discord.com/invite/zEM3skDU4r">join our discord for updates on the project</a>
       </header>
     </div>
   );
